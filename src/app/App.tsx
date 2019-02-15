a87a9ced7d588934cfcbefd852810687f1e8c0ac
danmakenoise <danmakenoise@gmail.com>
2019-02-15 00:33:18 -0500

    use a component from component ui for test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Text } from 'composite-ui';
 
 export default function App() {
-  return <h1>Composite</h1>;
+  return (
+    <div>
+      <h1>Composite</h1>
+      <Text>This is a test of the text library</Text>
+    </div>
+  );
 }
