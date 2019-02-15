ac00276ce96e3b8bb08cf5abc88d7a45ecedff09
danmakenoise <danmakenoise@gmail.com>
2019-02-14 23:48:42 -0500

    serve up public directory

--- src/server/start.ts
+++ src/server/start.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const server = express();
 
-server.get('/', (req, res) => res.send('Hello World!'));
+server.use(express.static('public'));
 
 server.listen(3000, () => console.log('Composite running on localhost:3000'));
